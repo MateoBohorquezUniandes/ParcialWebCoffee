b3abeb04777b0fa11ff9ce4a0d909244dc893f81
MateoBohorquezUniandes <i.bohorquezp@uniandes.edu.co>
2022-11-05 23:24:48 -0500

    Punto 1: Modulo Cafes

---
+++ src/app/Cafes/Cafes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Cafes',
+  templateUrl: './Cafes.component.html',
+  styleUrls: ['./Cafes.component.css']
+})
+export class CafesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/Cafes/Cafes.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CafesComponent } from './Cafes.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [CafesComponent]
+})
+export class CafesModule { }
